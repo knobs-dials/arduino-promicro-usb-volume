79e2134c17691023f93332780f5f9b7c7e1eac65
Bart <scarfboy@gmail.com>
2022-05-27 19:43:07 +0200

    Add files via upload

---
+++ volume.ino
@@ -0,0 +1,75 @@
+/*  USB volume knob with mute made from a 
+ *  - Arduino Pro Micro 
+ *  - HW-040 encoder
+ *  
+ *  Third-party libraries 
+ *  -  HID-Project
+ *  -  BasicEncoder
+ *  
+ *  Can repeat vol+ and vol- presses to go up/down faster, 
+ *    because the encoder is fairly coarse (30 is jumps) so is effectively a physical rate limit.
+ *  
+ *  By default also presses vol- more than vol+,
+ *    so that a "crap volume down" panic response works better,
+ *    and turning up is a little more precise
+ */
+#include <BasicEncoder.h>
+#include <TimerOne.h>
+#include <HID-Project.h>
+#include <HID-Settings.h>
+
+int up_nudges     = 3,
+    down_nudges   = 5,
+    button_state  = 0;  // state to avoid repeat presses, to ignore bounce on release, and to rate limit a little in the process
+
+BasicEncoder encoder( 14, 10 );      // the pins I happen to use
+
+void timer_service() {
+  encoder.service();
+}
+
+void setup() {
+  pinMode( 10, INPUT_PULLUP );
+  pinMode( 14, INPUT_PULLUP );
+  pinMode( 16, INPUT_PULLUP );
+  Consumer.begin();                  // https://github.com/NicoHood/HID/wiki/Consumer-API
+  Timer1.initialize( 1000 );         // 1kHz servicing the encoder
+  Timer1.attachInterrupt( timer_service );
+  //Serial.begin(115200); // only for debug
+}
+
+
+void loop() {
+  int encoder_change = encoder.get_count();
+  if (encoder_change != 0) {
+    //Serial.println( encoder_change );
+
+    if (encoder_change>0) {
+      for (char t = up_nudges;t;t--) {
+        Consumer.press(MEDIA_VOLUME_UP);
+        Consumer.release(MEDIA_VOLUME_UP);
+      }
+    }
+    if (encoder_change<0) {
+      for (char t = down_nudges;t;t--) {
+        Consumer.press(MEDIA_VOLUME_DOWN);
+        Consumer.release(MEDIA_VOLUME_DOWN);
+      }
+    }
+    encoder.reset();
+  }
+
+  char button = !digitalRead(16);
+  if (button) {
+    if (button_state == 0) {
+      button_state = 1000;
+      //Serial.println('M');    
+      Consumer.press(MEDIA_VOLUME_MUTE);
+      Consumer.release(MEDIA_VOLUME_MUTE);
+    } // else we're still ignoring it
+  } else { // button == 0
+    if (button_state > 0)
+      button_state--;
+  }
+  
+}
